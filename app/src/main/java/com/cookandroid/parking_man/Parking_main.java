04a4fd6bf7945cf8d91130738751abed14081c10
shimheejun <shimmyungsic@naver.com>
2020-07-24 16:43:50 +0900

    + 메인화면 자동완성기능 추가 + 선택후 해당페이지 이동 기능 추가

--- app/src/main/java/com/cookandroid/parking_man/Parking_main.java
+++ app/src/main/java/com/cookandroid/parking_man/Parking_main.java
@@ -1,29 +1,38 @@
 package com.cookandroid.parking_man;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class Parking_main extends AppCompatActivity {
 
+    private String[] items = {"현대백화점 무역센터점 (임시A)","현대백화점 앞구정점 (임시 B)"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.parking_main);
 
         Button btn_local_naxt,btn_search,btn_star;
-        AutoCompleteTextView autotext_search;
+        final AutoCompleteTextView autotext_search;
 
         btn_local_naxt = (Button)findViewById(R.id.btn_local_naxt);
         btn_search = (Button)findViewById(R.id.btn_search);
         btn_star = (Button)findViewById(R.id.btn_star);
         autotext_search = (AutoCompleteTextView) findViewById(R.id.autotext_search);
+
+
+        ArrayAdapter<String> adapter =new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,items);
+        autotext_search.setAdapter(adapter);
+
 
 
         // 지역선택 클릭 이벤트
@@ -41,6 +50,26 @@ public class Parking_main extends AppCompatActivity {
         btn_search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                String word = autotext_search.getText().toString();
+                if(word.equals(items[0])== true ) // 무역센터 선택시 이동
+                {
+                    Intent Intent = new Intent(getApplicationContext(),Parking_hyundai_mu_select.class);
+                    startActivity(Intent);
+                }
+                else if (word.equals(items[1])== true) // 앞구정점 선택시 이동
+                {
+                    Intent Intent = new Intent(getApplicationContext(),Parking_hyundai_ap_select.class);
+                    startActivity(Intent);
+                }
+                else // 다른값 넣을때 대화상자 출력
+                {
+                    AlertDialog.Builder dlg = new AlertDialog.Builder(Parking_main.this);
+                    dlg.setTitle("잘못된 검색");
+                    dlg.setMessage("검색된 주차장이 없습니다.");
+                    dlg.setPositiveButton("확인", null);
+                    dlg.show();
+                }
 
             }
         });
